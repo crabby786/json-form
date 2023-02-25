a45be369484f5525bc7edab80864df7087f9547a
vilask <vilask@iconnectsolutions.com>
2023-02-25 13:16:29 +0530

    test changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <a href="/tutorials" className="navbar-brand">
-          bezKoder
+          test app
         </a>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
